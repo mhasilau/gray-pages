0994d55ea1251d80aefae5dea66e6aa5ef493bb6
mhasilau <mikhail.hasilau@gmail.com>
2022-03-22 10:18:42 +0300

    lint-css lint prettier husky

--- src/app/pages/user-page/user-page.module.ts
+++ src/app/pages/user-page/user-page.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { UserPageComponent } from "./user-page.component";
 import { ContentModule } from "../../components/content/content.module";
-import { AvatarModule } from "../../components/avatar/avatar.module";
-import { NavMenuModule } from "../../components/nav-menu/nav-menu.module";
-import { SettingsModule } from "../../components/settings/settings.module";
+import { AvatarModule } from "../../components/content/avatar/avatar.module";
+import { NavMenuModule } from "../../components/content/nav-menu/nav-menu.module";
+import { SettingsModule } from "../../components/content/settings/settings.module";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
